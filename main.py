3371cc147d758a912302e1f3500e487f955b6dcb
CamHirsch23 <hirschboeck13@gmail.com>
2024-07-26 08:34:03 -0500

    Update and rename All.py to main.py

--- main.py
+++ main.py
@@ -18,3 +18,24 @@ mood = input("How are you feeling today? ")
 # Call the mood_response function and print the result
 print(mood_response(mood))
 
+# Question 2   
+# Task 1 
+
+# Simulating text_utils.py within the same file
+class text_utils:
+    @staticmethod
+    def reverse_string(s):
+        return s[::-1]
+
+    @staticmethod
+    def capitalize_string(s):
+        return s.capitalize()
+
+# Simulating main.py within the same file
+# Utilize the reverse_string function from text_utils
+reversed_text = text_utils.reverse_string("hello world")
+print(reversed_text)
+
+# Utilize the capitalize_string function from text_utils
+capitalized_text = text_utils.capitalize_string("hello world")
+print(capitalized_text)
